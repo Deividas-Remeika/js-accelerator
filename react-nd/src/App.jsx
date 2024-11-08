beaff34595740a07d62fac998cb8ed7613409502
Deividas Remeika <deividas.remeika1@gmail.com>
2024-11-08 12:33:36 +0200

    Move routes to router.js

--- react-nd/src/App.jsx
+++ react-nd/src/App.jsx
@@ -1,54 +1,6 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Services from "./pages/Services";
-import AboutUs from "./pages/AboutUs";
-import { ROUTES } from "./router/consts";
-import RootLayout from "./components/layout/RootLayout";
-import ErrorPage from "./pages/ErrorPage";
-import Login from "./pages/Login";
-import SearchCategory from "./pages/SearchCategory";
-import AuthLayout from "./components/layout/AuthLayout";
-import Register from "./pages/Register";
+import { RouterProvider } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
-
-const router = createBrowserRouter([
-  {
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: ROUTES.HOME,
-        element: <Home />,
-      },
-      {
-        path: ROUTES.SERVICES,
-        element: <Services />,
-      },
-      {
-        path: ROUTES.ABOUT_US,
-        element: <AboutUs />,
-      },
-      {
-        path: ROUTES.SEARCH_CATEGORY,
-        element: <SearchCategory />,
-      },
-    ],
-  },
-  {
-    element: <AuthLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: ROUTES.LOGIN,
-        element: <Login />,
-      },
-      {
-        path: ROUTES.REGISTER,
-        element: <Register />,
-      },
-    ],
-  },
-]);
+import router from "./router";
 
 const App = () => {
   return (
